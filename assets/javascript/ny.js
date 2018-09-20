eb669121e8c4c8e5e9704bbaf0ca0df490da35ca
Cheddi Charles <cheddi.charles@gmail.com>
2018-09-19 21:05:35 -0500

    added ny.js

---
+++ assets/javascript/ny.js
@@ -0,0 +1,13 @@
+var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+queryURL += '?' + $.param({
+  'api-key': "be2afc3fbee24813a76cdf43d3138d50"});
+// ajax method ( makes a call to the API)
+$.ajax({
+  url: queryURL,
+  method: "GET"
+})
+
+// promise resolve
+  .then(function(response) {
+      console.log(response);
+  })
